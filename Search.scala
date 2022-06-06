c3db52fb7c9485ed818405ae7045e480d074131e
cwholmes <cholmes5324@gmail.com>
2022-06-06 09:12:52 -0500

    Add possible dfs solution and fix bfs

--- Search.scala
+++ Search.scala
@@ -6,7 +6,7 @@ class BfsTraverser(start: Node, f: Node => Queue[Node]) {
   
   def traverse(): Stream[Node] = {
     def go(q: Queue[Node], visited: Set[Node]): Stream[Node] = {
-      if (q.isEmpty) collect(Stream())
+      if (q.isEmpty) Stream()
       else {
         val (node, rest) = q.dequeue
         if (visited.contains(node)) go(rest, visited)
@@ -14,5 +14,21 @@ class BfsTraverser(start: Node, f: Node => Queue[Node]) {
       }
     }
     go(Queue(start), Set())
+  }
+}
+
+class DfsTraverser(start: Node, f: Node => List[Node]) {
+
+  def traverse(): Stream[Node] = {
+    def go(stack: List[Node], visited: Set[Node]): Stream[Node] = {
+      if (q.isEmpty) Stream()
+      else {
+        stack.foldLeft(Stream()) { (stream, node) =>
+          // the stream will iterate through all possible paths for this node then move to the next
+          stream ++ (node #:: go(f(node), visited + node))
+        }
+      }
+    }
+    go(List(start), Set())
   }
 }
