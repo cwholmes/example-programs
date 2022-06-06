d1f9aea3f60ad0c31055af7050650c99b2ecbb72
cwholmes <cholmes5324@gmail.com>
2022-06-06 13:20:06 -0500

    Include a traversal with depth calculated

--- Search.scala
+++ Search.scala
@@ -16,6 +16,19 @@ class BfsTraverser[Node](start: Node, f: Node => Queue[Node]) {
     }
     go(Queue(start), Set())
   }
+  
+  def traverseWithDepth(): LazyList[(Node, Int)] = {
+    def go(q: Queue[(Node, Int)], visited: Set[Node]): LazyList[(Node, Int)] = {
+      q match {
+        case (node, depth) +: tail =>
+          if (visited.contains(node)) go(tail, visited)
+          // TODO: should visited always be tracked like this, would a node ever need to be met multiple times
+          else (node, depth) #:: go(tail ++ f(node).iterator.map(x => (x, depth + 1)), visited + node)
+        case _ => LazyList()
+      }
+    }
+    go(Queue((start, 0)), Set())
+  }
 }
 
 class DfsTraverser[Node](start: Node, f: Node => List[Node]) {
@@ -31,5 +44,18 @@ class DfsTraverser[Node](start: Node, f: Node => List[Node]) {
       }
     }
     go(List(start), Set())
+  }
+
+  def traverseWithDepth(): LazyList[(Node, Int)] = {
+    def go(stack: List[Node], visited: Set[Node], depth: Int): LazyList[(Node, Int)] = {
+      stack match {
+        case Nil => LazyList()
+        case node :: tail =>
+          // the stream will iterate through all possible paths for this node then move to the next
+          // TODO: Should this be able to track visited through all nodes or just this path to avoid a loop
+          (node, depth) #:: go(f(node), visited + node, depth + 1) #::: go(tail, visited, depth)
+      }
+    }
+    go(List(start), Set(), 0)
   }
 }
