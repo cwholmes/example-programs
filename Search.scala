781afe25776d4d4883c99bf4ba0b640aff7e2ef2
cwholmes <cholmes5324@gmail.com>
2022-06-06 12:07:28 -0500

    Simplify things with pattern matching

--- Search.scala
+++ Search.scala
@@ -6,12 +6,12 @@ class BfsTraverser[Node](start: Node, f: Node => Queue[Node]) {
   
   def traverse(): LazyList[Node] = {
     def go(q: Queue[Node], visited: Set[Node]): LazyList[Node] = {
-      if (q.isEmpty) LazyList()
-      else {
-        val (node, rest) = q.dequeue
-        if (visited.contains(node)) go(rest, visited)
-        // TODO: should visited always be tracked like this, would a node ever need to be met multiple times
-        else node #:: go(rest ++ f(node), visited + node)
+      q match {
+        case node +: tail =>
+          if (visited.contains(node)) go(tail, visited)
+          // TODO: should visited always be tracked like this, would a node ever need to be met multiple times
+          else node #:: go(tail ++ f(node), visited + node)
+        case _ => LazyList()
       }
     }
     go(Queue(start), Set())
@@ -22,13 +22,12 @@ class DfsTraverser[Node](start: Node, f: Node => List[Node]) {
 
   def traverse(): LazyList[Node] = {
     def go(stack: List[Node], visited: Set[Node]): LazyList[Node] = {
-      if (stack.isEmpty) LazyList()
-      else {
-        stack.foldLeft(LazyList[Node]()) { (stream, node) =>
+      stack match {
+        case Nil => LazyList()
+        case node :: tail =>
           // the stream will iterate through all possible paths for this node then move to the next
           // TODO: Should this be able to track visited through all nodes or just this path to avoid a loop
-          stream ++ (node #:: go(f(node), visited + node))
-        }
+          node #:: go(f(node), visited + node) #::: go(tail, visited)
       }
     }
     go(List(start), Set())
